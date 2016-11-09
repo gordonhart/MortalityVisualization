d9f15ee48ced4a00449a1f6374afbd77cc43240d
gordonhart <gordon.hart2@gmail.com>
2016-11-09 20:33:00 +0800

    Danger interactivity

--- viz/js/danger_ages.js
+++ viz/js/danger_ages.js
@@ -13,6 +13,9 @@ let plot_style = {
       size: 12
     }
   },
+  yaxis: {
+    range: [0,100]
+  },
   margin: {
     t: 150,
     b: 250
@@ -27,10 +30,10 @@ let plot_style = {
 let get_danger_data = () => {
   let ddata = danger_data.data[curage].causes;
   return [{
-    type: "scatter", // "bar",
+    type: "bar",
     mode: "markers",
     x: ddata.map((el) => el.cause),
-    y: ddata.map((el) => el.percent),
+    y: ddata.map((el) => 100 * el.percent),
     marker: {
       color: ddata.map((el) => el.percent)// ,
       // showscale: true
@@ -40,29 +43,59 @@ let get_danger_data = () => {
 };
 
 let render_danger_ages_initial = () => {
-  Plotly.plot("danger-ages",
-    get_danger_data(),
-    plot_style,
-    { showLink: false });
+  Plotly.plot("danger-ages", get_danger_data(), plot_style, { showLink: false })
+  /*
+  .then(() => {
+    Plotly.addFrames("danger-ages", danger_data.data.map((age,i) => {
+      let d = get_danger_data(i);
+      d.name = "frame" + i;
+      console.log("adding " + d.name);
+      return d;
+    }));
+  });
+  */
 };
 
+/*
+let start_animation = () => {
+  let frame_names = Array.apply(null, {length:danger_data.data.length}).map((el,i) => "frame" + i);
+  let frame_duration = Array.apply(null, {length:danger_data.data.length}).map(() => {return {duration:250}});
+  let frame_transition = Array.apply(null, {length:danger_data.data.length}).map(() => {return {duration:250,easing:"cubic-in"}});
+  Plotly.animate("danger-ages", frame_names, { frame: frame_duration, transition: frame_transition, mode: "afterall" });
+};
+*/
+
+
 let render_danger_ages_update = () => {
-  Plotly.animate("danger-ages", {
-    data: get_danger_data(), // [{ y: get_danger_data(danger_data.data[curage])[0].y }],
+  let data = get_danger_data();
+  let danger_div = document.getElementById("danger-ages");
+  danger_div.data = get_danger_data();
+
+  let scale_quantum = 20;
+  let maxy = Math.ceil(data[0].y.reduce((acc,el) => (el>acc) ? el : acc) / scale_quantum) * scale_quantum;
+  danger_div.layout.yaxis.range = [0,Math.max(maxy,2*scale_quantum)];
+
+  Plotly.redraw("danger-ages" /*, {
+    data: data,
     traces: [0],
-    layout: {}
+    layout: {
+      yaxis: {
+        range: [0,maxy]
+      }
+    }
   }, {
     transition: {
       duration: 250,
       ease: 'cubic-in-out'
     }
-  });
+  }); */);
 };
+
 
 let curage;
 let danger_data;
 $(() => {
-  let danger_age_json = "https://raw.githubusercontent.com/gordonhart/STAT3622/master/data/json/dangers_by_age_2014.json?token=AJM69pnP9Z8QuikbTCfUYT5PotoEwkr_ks5YKy8CwA%3D%3D";
+  let danger_age_json = "https://raw.githubusercontent.com/gordonhart/STAT3622/master/data/json/dangers_by_age_2014.json?token=AJM69meZC87OtbePBMIAhiYgK3-_Jf3Zks5YLDkAwA%3D%3D";
   $.get(danger_age_json, (dadata) => {
     danger_data = JSON.parse(dadata);
     curage = 0;
